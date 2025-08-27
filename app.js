4fb076764dc6507e359fa44446375f1374d64073
khabibkhanov <hamidulloxon2002@gmail.com>
2025-08-27 10:34:49 +0500

    updated file

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-require('dotenv').config()
-const express = require('express');
-const mongoose = require('mongoose');
-const fileUpload = require('express-fileupload')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
 // const postRoute = require('./router/post.route')
 
 const app = express();
-app.use(express.json())
-app.use(fileUpload({}))
+app.use(express.json());
+app.use(fileUpload({}));
 
 //Routes
-app.use('/api/post',require('./router/post.route'))
+app.use("/api/post", require("./src/router/post.route"));
 
+app.get("/", async (req, res) => {
+	try {
+		const allPosts = await postModel.find();
+		res.status(200).json(allPosts);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
 
+app.delete("/:id", (req, res) => {
+	const { id } = req.params;
+	res.send(id);
+});
+const PORT = process.env.PORT || 8080;
 
-app.get('/', async (req,res) =>{
-    try {
-        const allPosts = await postModel.find()
-        res.status(200).json(allPosts)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+app.put("/:id", (req, res) => {
+	const { id } = req.params;
+	const body = req.body;
+	res.json({ id, body });
+});
 
+const bootsrap = async () => {
+	try {
+		await mongoose
+			.connect(process.env.DB_URL)
+			.then(() => console.log("Connected DB"));
+		app.listen(PORT, () =>
+			console.log(`Listening on - http://localhost:${PORT}`)
+		);
+	} catch (error) {
+		console.log(`Error conncting with DB:${error}`);
+	}
+};
 
-
-app.delete("/:id", (req,res) =>{
-    const {id} = req.params
-    res.send(id)
-})
-const PORT = process.env.PORT || 8080
-
-app.put('/:id', (req,res) =>{
-    const {id} = req.params
-    const body = req.body
-    res.json({id, body})
-})
-
-const bootsrap = async () =>{
-    try{
-        await mongoose.connect(process.env.DB_URL).then(() => console.log("Connected DB"))
-        app.listen(PORT, () => console.log(`Listening on - http://localhost:${PORT}`))
-    }catch(error){
-        console.log(`Error conncting with DB:${error}`); 
-    }
-}
-
-bootsrap()
+bootsrap();
---
+++ src/controllers/post.controller.js
@@ -0,0 +1,53 @@
+const postModel = require("../models/post.model");
+const postService = require("../server/post.service");
+
+class PostController {
+	async getAll(req, res) {
+		try {
+			const allPosts = await postService.getAll();
+			res.status(200).json(allPosts);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	}
+
+	async create(req, res) {
+		try {
+			console.log(req.files);
+			const post = await postService.create(req.body, req.files.image);
+			res.status(201).json(post);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	}
+
+	async delete(req, res) {
+		try {
+			const post = await postService.delete(req.params.id);
+			res.status(200).json(post);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	}
+
+	async edit(req, res) {
+		try {
+			const { body, params } = req;
+			const post = await postService.edit(body, params.id);
+			res.status(200).json(post);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	async getOne(req, res) {
+		try {
+			const post = await postService.getOne(req.params.id);
+			res.status(200).json(post);
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	}
+}
+
+module.exports = new PostController();
